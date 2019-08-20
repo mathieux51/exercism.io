77715473ae76dfcc4ce6e8100e982d842709bd16
Mathieu <mathieux51@gmail.com>
2019-08-20 08:24:07 +0200

    WIP matrix

--- go/matrix/matrix.go
+++ go/matrix/matrix.go
@@ -9,7 +9,14 @@ type Matrix struct{}
 
 func New(s string) (*Matrix, error) {
 	rows := strings.Split(s, "\n")
-	fmt.Println(rows)
+	// var cols = 0
+
+	for _, row := range rows {
+		fmt.Println(row)
+	}
+
+	// fmt.Printf("len(rows): %v\n", len(rows))
+	// fmt.Printf("cols: %v\n", cols)
 	return nil, nil
 }
 
