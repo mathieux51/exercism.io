8eefb7820856610b5b0bf8c8c59956681ee82e25
Mathieu <mathieux51@gmail.com>
2019-09-06 07:39:30 +0200

    Update acronym

--- go/acronym/acronym.go
+++ go/acronym/acronym.go
@@ -1,19 +1,10 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
-// Package acronym should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
 package acronym
 
 import (
 	"strings"
 )
 
-func Split(r rune) bool {
-	return r == ' ' || r == '-'
-}
-
-// Abbreviate should have a comment documenting it.
+// Abbreviate ...
 func Abbreviate(s string) string {
 	s = strings.Replace(s, "-", " ", -1)
 	s = strings.Replace(s, "_", "", -1)
