8b855b0eb8b73e9035c1f710ea105d5ba144087e
Mathieu <mathieux51@gmail.com>
2019-09-06 08:00:58 +0200

    Solve hamming

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -1,4 +1,24 @@
 package hamming
 
+import (
+	"errors"
+	"strings"
+)
+
 func Distance(a, b string) (int, error) {
+	if len(a) != len(b) {
+		return 0, errors.New("sequences must have equal length")
+	}
+
+	var distance int
+	as := strings.Split(a, "")
+	bs := strings.Split(b, "")
+
+	for i := range as {
+		if as[i] != bs[i] {
+			distance += 1
+		}
+	}
+
+	return distance, nil
 }
