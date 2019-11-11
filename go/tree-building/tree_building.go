f5a3de8702e0cad2105f1aa47df3f135cf2f8a07
Mathieu <mathieux51@gmail.com>
2019-11-11 08:06:59 +0100

    WIP tree-building

--- go/tree-building/tree_building.go
+++ go/tree-building/tree_building.go
@@ -1,13 +1,18 @@
 package tree
 
+import (
+	"sort"
+)
+
 type Record struct {
 	ID     int
 	Parent int
 }
 
 type Node struct {
-	ID       int
-	Children []*Node
+	ID         int
+	IDChildren []*Node
+	Children   []*Node
 }
 
 func Build(records []Record) (*Node, error) {
@@ -15,8 +20,23 @@ func Build(records []Record) (*Node, error) {
 		return nil, nil
 	}
 
-	for record := range records {
-		return &Node{record.ID}
+	// Sort records
+	// Could be optimize by swaping
+	sort.SliceStable(records, func(i, j int) bool {
+		return records[i].ID < records[j].ID
+	})
+
+	nodes := make([]Node, len(records))
+
+	for i, r := range records {
+
+		nodes[i].ID = i
+		// if i != 0 {
+		// 	p := &nodes[r.Parent]
+		// 	p.Children = append(p.Children, &nodes[i])
+		// 	fmt.Printf("%+v\n", p)
+		// }
 	}
 
+	return &nodes[0], nil
 }
