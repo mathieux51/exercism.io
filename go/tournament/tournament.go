7eb0be1ab41839668a8860b3bed61190033ece72
Mathieu <mathieux51@gmail.com>
2020-05-20 07:30:07 +0200

    WIP Update tournament

--- go/tournament/tournament.go
+++ go/tournament/tournament.go
@@ -1,1 +1,22 @@
 package tournament
+
+import (
+	"fmt"
+	"io"
+)
+
+type Teams struct {
+	TeamName struct {
+		MP int `json:"MP"`
+		W  int `json:"W"`
+		D  int `json:"D"`
+		L  int `json:"L"`
+		P  int `json:"P"`
+	} `json:"team name"`
+}
+
+func Tally(r io.Reader, w io.Writer) error {
+	var inMemoryTally map[string]Teams
+	fmt.Println(inMemoryTally)
+	return nil
+}
